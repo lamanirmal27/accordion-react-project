372c8e24a332aae595437be88bdc4495fd71f9b1
Nirmal Tamang <lamanirmal27@gmail.com>
2024-04-21 12:09:23 +0545

    complete

---
+++ src/components/accordion/data.js
@@ -0,0 +1,23 @@
+const data = [
+    {
+        id: '1',
+        question: 'What are accordion components?',
+        answer: "The accordion component is used to generate vertically stacked content in a page. It is intended to save vertical space by hiding content, reducing scrolling. Accordions contains labels with the relevant information. Users can click on the accordion to read additional information."
+    }, 
+    {
+        id: '2 ',
+        question: 'What are they used for?',
+        answer: 'They are commonly employed in various contexts, including FAQs etc'
+    },
+    {
+        id: '3',
+        question: 'Accordion as a musical instrument',
+        answer: 'Accordions (from 19th-century German Akkordeon, from Akkord—"musical chord, concord of sounds") are a family of box-shaped musical instruments of the bellows-driven free reed aerophone type (producing sound as air flows past a reed in a frame).'
+    },
+    {
+        id: '4',
+        question: 'Can i create an accordion with a different from',
+        answer: 'Yes of course, it is very possible to create accordion components'
+    }
+]
+export default data
